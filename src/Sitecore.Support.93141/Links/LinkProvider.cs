62185a09d92e0064d31879c8ac03708cc67be9e6
Kirill Volovitskiy <kiv@sitecore.net>
2018-04-10 12:40:38 +0300

    port 443 is stripped from media and item URLs

---
+++ src/Sitecore.Support.93141/Links/LinkProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.Support.Links
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Links;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  public class LinkProvider : Sitecore.Links.LinkProvider
+  {
+    public override string GetItemUrl(Item item, UrlOptions options)
+    {
+      string url = base.GetItemUrl(item, options);
+
+      if (url.EndsWith(":443"))
+        return url.Replace(":443", "");
+
+      return url.Replace(":443/", "/");
+    }
+  }
+}
---
+++ src/Sitecore.Support.93141/Resources/Media/MediaProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Support.Resources.Media
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Resources.Media;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  public class MediaProvider : Sitecore.Resources.Media.MediaProvider
+  {
+    public override string GetMediaUrl(MediaItem item)
+    {
+      string url = base.GetMediaUrl(item);
+      return url.Replace(":443/", "/");
+    }
+
+    public override string GetMediaUrl(MediaItem item, MediaUrlOptions options)
+    {
+      string url = base.GetMediaUrl(item, options);
+      return url.Replace(":443/", "/");
+    }
+  }
+}
