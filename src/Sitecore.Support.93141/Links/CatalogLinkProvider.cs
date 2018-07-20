906b908af965543d96648e9f0aef4d50f5b95130
MichaelToh <ttk@sitecore.net>
2018-07-20 10:35:37 +0800

    Rework patch for SXC9.0.1

---
+++ src/Sitecore.Support.93141/Links/CatalogLinkProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.Commerce.XA.Foundation.Catalog.Pipelines
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Links;
+
+  public class CatalogLinkProvider : Sitecore.Commerce.XA.Foundation.Catalog.Pipelines.CatalogLinkProvider
+  {
+    public override string GetItemUrl(Item item, UrlOptions options)
+    {
+      string url = base.GetItemUrl(item, options);
+
+      if (url.EndsWith(":443"))
+        return url.Replace(":443", "");
+
+      return url.Replace(":443/", "/");
+    }
+  }
+}
